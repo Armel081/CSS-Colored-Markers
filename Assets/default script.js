d60474d63eda76cc7b30e54996076e99abdf2d52
Armel <xarmelx@gmail.com>
2022-05-23 00:26:48 -0400

    Added a new feature where you can click on the markers

---
+++ Assets/default script.js
@@ -0,0 +1,88 @@
+/*marker audio*/
+
+$(function() {  
+    const audio = new Audio("../Assets/Media/marker.wav" );
+$('.marker').click(function(){
+    audio.play();
+	});
+});
+
+const targets = 'h1, h2, p, a:not(.rainbow)'
+const classmap = {
+one: 'onetxt',
+two: 'twotxt',
+three: 'threetxt',
+four: 'fourtxt',
+five: 'fivetxt',
+six: 'sixtxt'
+}
+const allclasses = Object.values(classmap);
+
+function clearSelection() {
+allclasses.forEach(function(clz) { $(targets).removeClass(clz) });
+}
+
+function addSelection(sel) {
+    $(targets).addClass(classmap[sel]);
+}
+
+/*marker one*/
+
+$(function() {  
+    $('.one').click(function(){
+    clearSelection();
+    addSelection('one')
+    });
+});
+
+/*marker two*/
+
+
+$(function() {  
+    $('.two').click(function(){
+    clearSelection();
+    addSelection('two')
+    });
+});
+
+/*marker three*/
+
+
+$(function() {  
+    $('.three').click(function(){
+    clearSelection();
+    addSelection('three')
+    });
+});
+
+/*marker four*/
+
+
+$(function() {  
+    $('.four').click(function(){
+    clearSelection();
+    addSelection('four')
+    });
+});
+
+/*marker five*/
+
+
+$(function() {  
+    $('.five').click(function(){
+    clearSelection();
+    addSelection('five')
+    });
+});
+
+/*marker six*/
+
+
+$(function() {  
+    $('.six').click(function(){
+    clearSelection();
+    addSelection('six')
+    });
+});
+
+
